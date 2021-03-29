f7af6d5cdd0c03ef58d3f904eeeca10c438104e0
Pduhard <pacodj@free.fr>
2021-03-29 10:50:00 +0200

    LSTM on progress

--- Protodeep/layers/LSTM.py
+++ Protodeep/layers/LSTM.py
@@ -50,20 +50,21 @@ from Protodeep.layers.Layer import Layer
 class LSTM(Layer):
 
     total_instance = 0
-    weights = None
-    w_grad = None
+    # weights = None
+    # w_grad = None
 
-    biases = None
-    b_grad = None
+    # biases = None
+    # b_grad = None
 
-    a_val = None
-    z_val = None
+    # a_val = None
+    # z_val = None
     i_val = None
     dloss = None
 
     def __init__(self, units, activation='tanh', recurrent_activation='sigmoid',
                  use_bias=True, kernel_initializer='glorot_uniform',
-                 recurrent_initializer='orthogonal',
+                 recurrent_initializer='glorot_uniform',
+                #  recurrent_initializer='orthogonal',
                  bias_initializer='zeros', unit_forget_bias=True,
                  kernel_regularizer=None, recurrent_regularizer=None,
                  bias_regularizer=None, activity_regularizer=None,
@@ -97,29 +98,44 @@ class LSTM(Layer):
     def __call__(self, connectors):
         # if isinstance(connectors.shape, tuple):
         #     connectors.shape = connectors.shape[-1]
-        # print(connectors.shape[1:])
+        print(connectors.shape)
+        timesteps, feature = connectors.shape
 
-        weight_shape = (connectors.shape[-1] + self.units, self.units)
+        weight_shape = (feature, self.units)
+        hidden_weight_shape = (self.units, self.units)
+        # gate_shape = (timesteps, )
+
         # print(weight_shape)
         # quit()
         self.forget_weight = self.recurrent_initializer(weight_shape)
-        self.memory_weight = self.recurrent_initializer(weight_shape)
-        self.input_weight = self.kernel_initializer(weight_shape)
+        self.input_weight = self.recurrent_initializer(weight_shape)
         self.output_weight = self.recurrent_initializer(weight_shape)
+        self.candidate_weight = self.kernel_initializer(weight_shape)
+
+        self.forget_hidden_weight = self.recurrent_initializer(hidden_weight_shape)
+        self.input_hidden_weight = self.recurrent_initializer(hidden_weight_shape)
+        self.output_hidden_weight = self.recurrent_initializer(hidden_weight_shape)
+        self.candidate_hidden_weight = self.kernel_initializer(hidden_weight_shape)
+        
         self.forget_biase = self.bias_initializer(self.units)
-        self.memory_biase = self.bias_initializer(self.units)
         self.input_biase = self.bias_initializer(self.units)
         self.output_biase = self.bias_initializer(self.units)
+        self.candidate_biase = self.bias_initializer(self.units)
         # self.weights = self.kernel_initializer(weight_shape)
         self.forget_w_grad = np.zeros(weight_shape)
-        self.memory_w_grad = np.zeros(weight_shape)
         self.input_w_grad = np.zeros(weight_shape)
         self.output_w_grad = np.zeros(weight_shape)
+        self.candidate_w_grad = np.zeros(weight_shape)
+
+        self.forget_hidden_w_grad = np.zeros(hidden_weight_shape)
+        self.input_hidden_w_grad = np.zeros(hidden_weight_shape)
+        self.output_hidden_w_grad = np.zeros(hidden_weight_shape)
+        self.candidate_hidden_w_grad = np.zeros(hidden_weight_shape)
 
         self.forget_b_grad = np.zeros(self.units)
-        self.memory_b_grad = np.zeros(self.units)
         self.input_b_grad = np.zeros(self.units)
         self.output_b_grad = np.zeros(self.units)
+        self.candidate_b_grad = np.zeros(self.units)
         # self.biases = self.bias_initializer(self.units)
         # self.b_grad = np.zeros(self.units)
 
@@ -133,13 +149,13 @@ class LSTM(Layer):
         """ input must be a 1d array """
         print('ha')
         quit()
-        if isinstance(input_shape, tuple):
-            input_shape = input_shape[-1]
-        weight_shape = (input_shape, self.units)
-        self.weights = self.kernel_initializer(weight_shape)
-        self.w_grad = np.empty(weight_shape)
-        self.biases = self.bias_initializer(self.units)
-        self.b_grad = np.empty(self.units)
+        # if isinstance(input_shape, tuple):
+        #     input_shape = input_shape[-1]
+        # weight_shape = (input_shape, self.units)
+        # self.weights = self.kernel_initializer(weight_shape)
+        # self.w_grad = np.empty(weight_shape)
+        # self.biases = self.bias_initializer(self.units)
+        # self.b_grad = np.empty(self.units)
         # self.dloss = np.empty()
         
 
@@ -164,12 +180,60 @@ class LSTM(Layer):
 
     def forward_pass(self, inputs):
         print('forward lstm')
-        # print(inputs.shape)
-        quit()
+        print(inputs.shape)
+        self.i_val = inputs
+        # self.forget_gate.fill(0)
+
+        batch_size, timestep, feature = inputs.shape 
+        self.forget_gate = np.zeros(shape=(batch_size, timestep + 1, self.units))
+        self.input_gate = np.zeros(shape=(batch_size, timestep + 1, self.units))
+        self.output_gate = np.zeros(shape=(batch_size, timestep + 1, self.units))
+        self.candidate_gate = np.zeros(shape=(batch_size, timestep + 1, self.units))
+
+        self.hidden_state = np.zeros(shape=(batch_size, timestep + 1, self.units))
+        self.cell_state = np.zeros(shape=(batch_size, timestep + 1, self.units))
+        self.a_val = np.zeros(shape=(batch_size, timestep + 1, self.units))
+        for b, inpt in enumerate(inputs):
+            i = 1
+            for x in inpt:
+                print(np.sum(np.matmul(x, self.forget_weight))
+                    ,np.sum(np.matmul(self.hidden_state[b, i - 1], self.forget_hidden_weight)))
+                self.forget_gate[b, i] = self.recurrent_activation(
+                    np.matmul(x, self.forget_weight)
+                    + np.matmul(self.hidden_state[b, i - 1], self.forget_hidden_weight)
+                    + self.forget_biase
+                )
+                self.input_gate[b, i] = self.recurrent_activation(
+                    np.matmul(x, self.input_weight)
+                    + np.matmul(self.hidden_state[b, i - 1], self.input_hidden_weight)
+                    + self.input_biase
+                )
+                self.output_gate[b, i] = self.recurrent_activation(
+                    np.matmul(x, self.output_weight)
+                    + np.matmul(self.hidden_state[b, i - 1], self.output_hidden_weight)
+                    + self.output_biase
+                )
+                self.candidate_gate[b, i] = self.activation(
+                    np.matmul(x, self.candidate_weight)
+                    + np.matmul(self.hidden_state[b, i - 1], self.candidate_hidden_weight)
+                    + self.candidate_biase
+                )
+
+                self.cell_state[b, i] = self.cell_state[b, i - 1] * self.forget_gate[b, i]
+                + self.input_gate[b, i] * self.candidate_gate[b, i]
+
+                self.hidden_state[b, i] = self.activation(self.cell_state[b, i]) * self.output_gate[b, i]
+                self.a_val[b, i] += self.activation(self.cell_state[b, i]) * self.output_gate[b, i]
+                i += 1
+                # np.matmul(x, self.forget_weight, out=self.forget_gate[b, i])
+                # print(np.matmul(x, self.forget_weight) == self.forget_gate[b, i])
+                # print(np.matmul(x, self.forget_weight).shape)
+                # self.forget_gate[i]
         # self.i_val = inputs
         # self.z_val = np.dot(inputs, self.weights) + self.biases
         # self.a_val = self.activation(self.z_val)
-        # print(self.a_val)
+        print(self.a_val[1])
+        quit()
         return self.a_val
 
     def backward_pass(self, inputs):
@@ -205,26 +269,34 @@ class LSTM(Layer):
     def get_trainable_weights(self):
         return [
             self.forget_weight,
-            self.memory_weight,
             self.input_weight,
             self.output_weight,
+            self.candidate_weight,
+            self.forget_hidden_weight,
+            self.input_hidden_weight,
+            self.output_hidden_weight,
+            self.candidate_hidden_weight,
             self.forget_biase,
-            self.memory_biase,
             self.input_biase,
-            self.output_biase
+            self.output_biase,
+            self.candidate_biase
         ]
 
 
     def get_gradients(self):
         return [
             self.forget_w_grad,
-            self.memory_w_grad,
             self.input_w_grad,
             self.output_w_grad,
+            self.candidate_w_grad,
+            self.forget_hidden_w_grad,
+            self.input_hidden_w_grad,
+            self.output_hidden_w_grad,
+            self.candidate_hidden_w_grad,
             self.forget_b_grad,
-            self.memory_b_grad,
             self.input_b_grad,
-            self.output_b_grad
+            self.output_b_grad,
+            self.candidate_b_grad
         ]
     
     def set_weights(self, weights):
