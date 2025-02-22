78b7f33e12131565f0f1c621dec20427b3a18fcd
Peaupoosh <pacodj@free.fr>
2025-02-22 16:04:07 +0100

    chore: cleanup commented code in dataset.py

--- Examples/dataset.py
+++ Examples/dataset.py
@@ -1,6 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
-# from numba import njit
 
 from fstat import Fstat
 
@@ -8,12 +6,9 @@ def time_series_split(features, targets, ssize=10):
     wfeatures = []
     wtargets = []
 
-
     for i in range(len(targets) - ssize):
         wfeatures.append(features[i:i+ssize])
         wtargets.append(targets[i+ssize][0])
-    # for d in dataset[:10]:
-    #     print(d)
     wfeatures = np.array(wfeatures).astype(np.float32)
     wtargets = np.array(wtargets).astype(np.float32)
     return wfeatures, wtargets
@@ -27,14 +22,9 @@ def parse_btc(file_name='BTCUSD_day.csv'):
         lines.pop(0)
         for i, line in enumerate(lines):
             sline = line.split(",")[3:-1]
-            # print(sline[-1])
-            # target = sline.pop(0)
             targets[i] = float(sline[-1])
             features[i] = np.array(sline, dtype=float)
-        # print(features.shape)
-        # quit()
     return features, targets[:, np.newaxis]
-    return time_series_split(features, targets[:, np.newaxis], ssize=10)
 
 
 def parse_csv(file_name):
@@ -42,9 +32,6 @@ def parse_csv(file_name):
     lines = [line for line in fd.read().split() if len(line) > 1]
     features = np.empty((len(lines), 30))
     targets = np.empty((len(lines), 2))
-    # print (features.shape)
-    # print (targets.shape)
-    # i = 0
     for i, line in enumerate(lines):
         sline = line.split(",")[1:]
         target = sline.pop(0)
@@ -57,41 +44,17 @@ def parse_mnist_csv(file_name):
     fd = open(file_name, "r")
     lines = fd.read().split()[:]
     _ = lines.pop(0)
-    # print(header)
-    # print(len(header.split(",")))
-    # print(len(lines))
-    # quit()
     features = np.empty((len(lines), 28, 28, 1))
     targets = np.zeros((len(lines), 10))
-    # print (features.shape)
-    # print (targets.shape)
     for i, line in enumerate(lines):
         sline = line.split(",")
         target = int(sline.pop(-1))
         targets[i][target] = 1
-        # targets[i] = [1, 0] if target == "M" else [0, 1]
         features[i] = np.array(sline).reshape((28, 28, 1))
-        # print(features[0])
-        # print(targets[0])
-    # plt.imshow(features[0], cmap=plt.get_cmap('gray'))
-    # plt.show()
     return (features.astype(float) / 255., targets.astype(float))
-
-# https://medium.com/@pavisj/convolutions-and-backpropagations-46026a8f5d2c
-
 
 seed = 303
 epsilon = 1e-8
-
-
-# @njit
-def _standardize(features, features_stat):
-
-    for feature in features:
-        for i, f in enumerate(feature):
-            fs = features_stat[i]
-            feature[i] = (f - fs.mean) / (fs.std)
-
 
 class Dataset:
 
@@ -99,11 +62,9 @@ class Dataset:
     test_targets = None
 
     def split_dataset(self, test_split):
-        # rng_state = np.random.get_state()
         np.random.seed(seed)
         np.random.shuffle(self.features)
         np.random.seed(seed)
-        # np.random.set_state(rng_state)
         np.random.shuffle(self.targets)
         split = int((1 - test_split) * len(self.features))
         self.test_features = self.features[split:]
@@ -126,9 +87,6 @@ class Dataset:
             self.features_stat = [Fstat(feature) for feature in self.features.T]
             self.standardize()
             self.split_dataset(test_split)
-        # plt.imshow(self.features[0], cmap=plt.get_cmap('gray'))
-        # plt.show()
-        # quit()
 
     def normalize(self):
         for feature in self.features:
@@ -137,11 +95,8 @@ class Dataset:
                 feature[i] = (f - fs.min) / (fs.max - fs.min)
 
     def standardize(self):
-        # _standardize(self.features, self.features_stat)
         for feature in self.features:
             for i, f in enumerate(feature):
                 fs = self.features_stat[i]
-                # print(fs.std)
                 feature[i] = (f - fs.mean) / (fs.std)
                 feature[i] = (f - fs.mean) / (fs.std + epsilon)
-                # print(feature[i])
